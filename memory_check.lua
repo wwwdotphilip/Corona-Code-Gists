7c630d4f87f89959bfd3eddcb43787e3ddf814df
John Philip Agustino <wwwdotphilip@gmail.com>
2013-08-12 10:54:41 +0800

    Update memory_check.lua

--- memory_check.lua
+++ memory_check.lua
@@ -1,9 +1,10 @@
 local function checkmem()
     collectgarbage()
-    print( "MemUsage: " .. string.format("%.00f",collectgarbage("count")) .. " KB" )
+    print( "MemUsage: " .. string.format("%.03f",collectgarbage("count")) .. " KB" )
     
     local textMem = system.getInfo( "textureMemoryUsed" ) / 1048576
     print( "TexMem:   " .. string.format("%.03f", textMem) .. " MB" )
 end
 
-timer.performWithDelay(3000, checkmem, 0)
+timer.performWithDelay(3000, checkmem, 0) -- call function every 3 seconds.
+                                          -- you can change the interval if you want.
