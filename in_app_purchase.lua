76cd8e8d8e78176c7371df9ebaf574622683cfc2
John Philip Agustino <wwwdotphilip@gmail.com>
2013-08-15 17:00:21 +0800

    Update in_app_purchase.lua

--- in_app_purchase.lua
+++ in_app_purchase.lua
@@ -1,1 +1,47 @@
---
+local store = require"store" -- initialize store api.
+
+-- You can let this as it is. If you want to see more explaination about his visit the Corona API
+-- http://docs.coronalabs.com/guide/monetization/IAP/index.html#transaction-listener
+local function transactionCallback( event )
+    print("transactionCallback: Received event " .. tostring(event.name))
+    print("state: " .. tostring(event.transaction.state))
+    print("errorType: " .. tostring(event.transaction.errorType))
+    print("errorString: " .. tostring(event.transaction.errorString))
+    
+    local productID= event.transaction.productIdentifier;
+    if event.transaction.state == "purchased" then
+        print("Product Purchased: ", productID)
+    elseif  event.transaction.state == "restored" then
+        print("Product Restored", productID)
+    elseif  event.transaction.state == "refunded" then
+        print("Product Refunded")
+    elseif event.transaction.state == "cancelled" then
+        print("Transaction cancelled")
+    elseif event.transaction.state == "failed" then        
+        print("Transaction Failed")
+    else
+        print("Some unknown event occured.  This should never happen.")
+    end
+    store.finishTransaction( event.transaction )
+end 
+
+
+-- Identifies the device and will initialize according to type.
+if store.availableStores.apple then
+    store.init("apple", transactionCallback)
+elseif store.availableStores.google then
+    store.init("google", transactionCallback)
+end
+
+
+-- this handles the purchasing transaction.
+local function purchaseItem()
+    -- make sure that your iOS and Google In-App Products has the same product ID
+    store.purchase( {"eight.app.studio.myappname.myproductname"})
+end
+
+-- When a user buys new phone or uninstall your app but reinstalls it later
+-- all purchased products will be restored.
+local function restorePurchases()
+    store.restore()
+end
