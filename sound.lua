04b182475cbf90104a850660277e689415a06652
John Philip Agustino <wwwdotphilip@gmail.com>
2013-08-20 10:19:44 +0800

    Update sound.lua

--- sound.lua
+++ sound.lua
@@ -1,22 +1,22 @@
-local media = require"media"
+local media = require"media";
 
 local soundSet = {
     shortSound1 = media.newEventSound("sounds/shortSound1.wav"),
     shortSound2 = media.newEventSound("sounds/shortSound2.wav"),
-    bgSound = audio.loadStream("sounds/bgSound.mp3")
-}
+    bgSound = audio.loadStream("sounds/bgSound.wav")
+};
 
 local function playShortSound1()
-    media.playEventSound( soundSet["shortSound1"] )
+    media.playEventSound( soundSet["shortSound1"] );
 end
-timer.performWithDelay(1000, playShortSound1)
+timer.performWithDelay(1000, playShortSound1);
 
 local function playShortSound2()
-    media.playEventSound( soundSet["shortSound2"] )
+    media.playEventSound( soundSet["shortSound2"] );
 end
-timer.performWithDelay(2000, playShortSound2)
+timer.performWithDelay(2000, playShortSound2);
 
 local function playbgSound()
-    audio.play(soundSet["bgSound"])
+    audio.play(soundSet["bgSound"]);
 end
-timer.performWithDelay(3000, playbgSound)
+timer.performWithDelay(3000, playbgSound);
