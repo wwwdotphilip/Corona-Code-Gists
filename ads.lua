49c4e33b85a01ff9cdfbeaf6d43d4899c68fa657
JohnDev <johna@standardinternetmarketing.com>
2013-11-07 14:17:16 +0800

    Organized ads library

--- ads.lua
+++ ads.lua
@@ -17,9 +17,9 @@
 
     ads.callchartboost("fullscreen")
     
-    ]]
-    
-    local ads = {}
+]]
+
+local ads = {}
 
 local AMAZON_APK = false
 
@@ -31,11 +31,11 @@ local revmob_ANDROID = "APP ID REVMOB"
 local revmob_IOS = "APP ID REVMOB"
 local revmob_AMAZON = "APP ID REVMOB"
 
-    local REVMOB_IDS = { ["Android"] = revmob_ANDROID, ["iPhone OS"] = revmob_IOS }
-    if AMAZON_APK then REVMOB_IDS[REVMOB_ID_ANDROID] = revmob_AMAZON end
+local REVMOB_IDS = { ["Android"] = revmob_ANDROID, ["iPhone OS"] = revmob_IOS }
+if AMAZON_APK then REVMOB_IDS[REVMOB_ID_ANDROID] = revmob_AMAZON end
 RevMob.startSession(REVMOB_IDS)
 
-    local banner_revmob = RevMob.createBanner({x = display.contentWidth / 2, y = _H - 50, width = _W, height = 100 })
+local banner_revmob = RevMob.createBanner({x = display.contentWidth / 2, y = _H - 50, width = _W, height = 100 })
 banner_revmob:hide()
 
 local fullscreen_revmob = RevMob.createFullscreen()
@@ -55,8 +55,8 @@ function ads.callrevmob (action)
         fullscreen_revmob:hide()
     elseif action == "show_pop" then
         show_pop:show()
-        end
     end
+end
 
 local TOP = 1
 local CENTER = 2
@@ -78,9 +78,9 @@ local function adViewListener(event)
         tapfortap.removeAdView()
     elseif event.event == "onFailToReceiveAd" then
         --if tapfortap banner fail, put alternative ads revmob banner
-        end
-    
     end
+    
+end
 
 tapfortap.setAdViewListener(adViewListener)
 
@@ -91,8 +91,8 @@ local function interstitialListener(event)
         --trigger for ads tapped cause
     elseif event.event == "onFailToReceiveAd" then
         --if tapfortap fullscreen fail, put alternative ads revmob fullscreen
-        end
     end
+end
 
 tapfortap.setInterstitialListener(interstitialListener)
 
@@ -109,8 +109,8 @@ function ads.calltapfortap(action)
         tapfortap.removeAdView()
     elseif action == "more_games" then
         tapfortap.showAppWall()
-        end
     end
+end
 
 local cbdata = require "ChartboostSDK.chartboostdata"
 local cb = require "ChartboostSDK.chartboost"
@@ -121,31 +121,31 @@ local appBundle = "APP PACKAG"
 local appVersion = "APP VERSION"
 
 local delegate = {
-        shouldRequestInterstitial = function(location) print("Chartboost: shouldRequestInterstitial " .. location .. "?"); return true end,
-        shouldDisplayInterstitial = function(location) print("Chartboost: shouldDisplayInterstitial " .. location .. "?"); return true end,
-        didCacheInterstitial = function(location) print("Chartboost: didCacheInterstitial " .. location); return end,
-        didFailToLoadInterstitial = function(location) print("Chartboost: didFailToLoadInterstitial " .. location); return end,
-        didDismissInterstitial = function(location) print("Chartboost: didDismissInterstitial " .. location); return end,
-        didCloseInterstitial = function(location) print("Chartboost: didCloseInterstitial " .. location); return end,
-        didClickInterstitial = function(location) print("Chartboost: didClickInterstitial " .. location); return end,
-        didShowInterstitial = function(location) print("Chartboost: didShowInterstitial " .. location); return end,
-        shouldDisplayLoadingViewForMoreApps = function() return true end,
-        shouldRequestMoreApps = function() print("Chartboost: shouldRequestMoreApps"); return true end,
-        shouldDisplayMoreApps = function() print("Chartboost: shouldDisplayMoreApps"); return true end,
-        didCacheMoreApps = function() print("Chartboost: didCacheMoreApps"); return end,
-        didFailToLoadMoreApps = function() print("Chartboost: didFailToLoadMoreApps"); return end,
-        didDismissMoreApps = function() print("Chartboost: didDismissMoreApps"); return end,
-        didCloseMoreApps = function() print("Chartboost: didCloseMoreApps"); return end,
-        didClickMoreApps = function() print("Chartboost: didClickMoreApps"); return end,
-        didShowMoreApps = function() print("Chartboost: didShowMoreApps"); return end,
-        shouldRequestInterstitialsInFirstSession = function() return true end
-    }
+    shouldRequestInterstitial = function(location) print("Chartboost: shouldRequestInterstitial " .. location .. "?"); return true end,
+    shouldDisplayInterstitial = function(location) print("Chartboost: shouldDisplayInterstitial " .. location .. "?"); return true end,
+    didCacheInterstitial = function(location) print("Chartboost: didCacheInterstitial " .. location); return end,
+    didFailToLoadInterstitial = function(location) print("Chartboost: didFailToLoadInterstitial " .. location); return end,
+    didDismissInterstitial = function(location) print("Chartboost: didDismissInterstitial " .. location); return end,
+    didCloseInterstitial = function(location) print("Chartboost: didCloseInterstitial " .. location); return end,
+    didClickInterstitial = function(location) print("Chartboost: didClickInterstitial " .. location); return end,
+    didShowInterstitial = function(location) print("Chartboost: didShowInterstitial " .. location); return end,
+    shouldDisplayLoadingViewForMoreApps = function() return true end,
+    shouldRequestMoreApps = function() print("Chartboost: shouldRequestMoreApps"); return true end,
+    shouldDisplayMoreApps = function() print("Chartboost: shouldDisplayMoreApps"); return true end,
+    didCacheMoreApps = function() print("Chartboost: didCacheMoreApps"); return end,
+    didFailToLoadMoreApps = function() print("Chartboost: didFailToLoadMoreApps"); return end,
+    didDismissMoreApps = function() print("Chartboost: didDismissMoreApps"); return end,
+    didCloseMoreApps = function() print("Chartboost: didCloseMoreApps"); return end,
+    didClickMoreApps = function() print("Chartboost: didClickMoreApps"); return end,
+    didShowMoreApps = function() print("Chartboost: didShowMoreApps"); return end,
+    shouldRequestInterstitialsInFirstSession = function() return true end
+}
 
 cb.create{appId = appId,
     appSignature = appSignature,
     delegate = delegate,
     appVersion = appVersion,
-    appBundle = appBundle}
+appBundle = appBundle}
 cb.startSession()
 
 cb.cacheMoreApps() -- load more app's before showing
@@ -156,8 +156,8 @@ function ads.callchartboost(action)
         cb.showMoreApps() --find didFailToLoadMoreApps in delegate to have backup more apps if chartboost fail to load
     elseif action == "fullscreen" then
         cb.showInterstitial() --find didFailToLoadInterstitial in delegate to have backup fullscreen ads if chartboost fail to load
-        end
     end
+end
 
 return ads
 
