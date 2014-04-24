72a733aeda11e9477a0a8c75149c2ea4836edb19
John Philip Agustino <wwwdotphilip@gmail.com>
2014-04-24 09:33:53 +0800

    New updates for config.lua

--- config.lua
+++ config.lua
@@ -3,36 +3,24 @@
 --
 -- example: Place this code inside your main.lua to try.
 -- 
--- local _W = display.contentWidth
--- local _H = display.contentHeight
+-- local W = display.contentWidth
+-- local H = display.contentHeight
 -- local rect = display.newRect(40, 40, 80, 80)
 -- rect.x = _W / 2; rect.y = _H / 2; -- this will place the rectangle in the middle
 --                                   -- no matter what the screen size is.
-
+local h
 if display.pixelHeight > 960 then -- for device with longer height e.g. iPhone 5 and most Android Devices.
-    application = {
-        content = {
-            width = 640,
-            height = 1136,
-            scale = "zoomStretch", -- Stretch to fit entire screen. This will cause some objects to look flat or stretched.
-            fps = 30,              -- But I advice to use this scale since some app stores rejects apps that will not display
-            antialias = "true",    -- all objects properly.
-        },
-        imageSuffix = {
-            ["@2x"] = 2,           -- For iPhone retina display. Apple requires that all apps support retina display.
-        },
-    }
-else -- for devices with shorter height e.g. iPhone 4, 3GS, iPad, some Android phones.
-    application = {
-        content = {
-            width = 640,
-            height = 960,
-            scale = "zoomStretch",
-            fps = 30,
-            antialias = "true",
-        },
-        imageSuffix = {
-            ["@2x"] = 2,
-        },
-    }
+    h = 1136
 end
+application = {
+    content = {
+        width = 640,
+        height = h or 960,
+        scale = "zoomStretch", -- Stretch to fit entire screen. This will cause some objects to look flat or stretched.
+        fps = 30,              -- But I advice you to use this scale since some app stores rejects apps that will not display
+        antialias = "true",    -- all objects properly.
+    },
+    imageSuffix = {
+        ["@2x"] = 2,           -- For iPhone retina display. Apple requires that all apps support retina display.
+    },
+}
